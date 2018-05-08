4a9fbafcd29c7fac71ef9117228fc98638161983
Alex Radov <s1makrdv@gmail.com>
2018-05-08 11:40:32 +0300

    Update firstDuplicate.cpp

--- CodeFights/firstDuplicate.cpp
+++ CodeFights/firstDuplicate.cpp
@@ -42,3 +42,16 @@ int main() {
 	system("pause");
 	return 0;
 }
+
+//Best solution is:
+/*
+int firstDuplicate(std::vector<int> a) {
+    for(int i=0; i < a.size(); i++){
+        int t = abs(a[i]);
+        if(a[t - 1] < 0)
+            return t;
+        a[t - 1] = - a[t - 1];
+    }
+    return -1;
+}
+*/
