c8a930c2ec185faedd1bc29d3056886fe969f32a
Alex Radov <s1makrdv@gmail.com>
2018-05-08 15:18:25 +0300

    Update rotateImage.cpp

--- CodeFights/rotateImage.cpp
+++ CodeFights/rotateImage.cpp
@@ -42,3 +42,20 @@ int main()
    system("pause");
    return 0;
 }
+
+/*Best Solve
+std::vector<std::vector<int>> rotateImage(std::vector<std::vector<int>> a) {
+    int n = a.size();
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = i; j < n-i-1; ++j)
+        {
+            swap(a[i][j],a[j][n-i-1]);
+            swap(a[i][j],a[n-i-1][n-j-1]);
+            swap(a[i][j],a[n-j-1][i]);
+        }
+    }
+    return a;
+}
+
+*/
