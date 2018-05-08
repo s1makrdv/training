61f8bfda7fd3537888d0178b433fe451a4d92467
Alex Radov <s1makrdv@gmail.com>
2018-05-08 15:16:50 +0300

    Create rotateImage.cpp

---
+++ CodeFights/rotateImage.cpp
@@ -0,0 +1,44 @@
+//You are given an n x n 2D matrix that represents an image.Rotate the image by 90 degrees(clockwise).
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+void printMatrix(const vector<vector<int>> a)
+{
+   for (auto i : a) {
+      for (auto j : i) {
+         cout << setw(2) << j << ' ';
+      }
+      cout << endl;
+   }
+}
+
+vector<vector<int>> rotateImage(vector<vector<int>> a) 
+{
+   vector<vector<int>> v(a);
+   size_t size = v[0].size();
+   int k = 0;
+   for (int i = 0; i <= (size - 1); i++) {
+      for (int j = i + 1; j < (size - i); j++) {
+         k = v[j][size - i - 1];
+         v[j][size - i - 1] = v[i][j];
+         v[i][j]            = v[size - j - 1][i];
+         v[size - j - 1][i] = v[size - 1 - i][size - 1 - j];
+         v[size - 1 - i][size - 1 - j] = k;
+      }
+   }
+   return v;
+}
+
+int main()
+{
+   vector<vector<int>> matrix = { {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16} };
+   printMatrix(matrix);
+   cout << endl;
+   printMatrix(rotateImage(matrix));
+   system("pause");
+   return 0;
+}
